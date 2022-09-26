bdcc1547d002fdd80e213fafb32fea8e82577708
abdou_dev <abdouou7@gmail.com>
2022-09-26 06:56:25 +0100

    read from std

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+
+fn init() {
+    println!("Guess the number");
+    println!("Please input your guess")
+}
+
+fn start() {
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    print!("You guessed {guess}");    
+}
+
 fn main() {
-    println!("Hello, world!");
+    init();
+    start();
 }

8042c0f9c8b20fe290a0694b9f142bb60d815d97
abdou_dev <abdouou7@gmail.com>
2022-09-26 07:53:29 +0100

    install rand and use match

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::io;
+use rand::Rng;
+use std::{cmp::Ordering, io};
 
 fn init() {
     println!("Guess the number");
@@ -7,10 +8,22 @@ fn init() {
 
 fn start() {
     let mut guess = String::new();
+    let random_number = rand::thread_rng().gen_range(1..=100);
+    // println!("The secret number is {random_number}");
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
-    print!("You guessed {guess}");    
+    print!("You guessed {guess}");
+    compare(random_number, guess)
+}
+
+fn compare(random_number: i32, guess: String) {
+    let guess: i32 = guess.trim().parse().expect("Should be a number");
+    match guess.cmp(&random_number) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
 
 fn main() {
